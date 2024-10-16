8dbee254e3cf67a068f1e397603a51dc0e69e47c
Jerome Otto <jeromeo1969@hotmail.com>
2024-10-16 16:34:21 -0600

    Refactor HousesController and add dependencies

---
+++ Controllers/HousesController.cs
@@ -0,0 +1,16 @@
+namespace gregslistCSharp.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HousesController : ControllerBase
+{
+  public HousesController(HousesService housesService, Auth0Provider auth0Provider)
+  {
+    _housesService = housesService;
+    _auth0Provider = auth0Provider;
+  }
+
+  private readonly HousesService _housesService;
+  private readonly Auth0Provider _auth0Provider;
+
+}
